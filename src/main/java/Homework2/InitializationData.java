7c5c804f6ee885aeb0b6f41f81755aa0a48f2c26
Oleksandr <alexanderch87@gmail.com>
2020-10-20 20:42:49 +0300

    HT2

--- src/main/java/Homework2/InitializationData.java
+++ src/main/java/Homework2/InitializationData.java
@@ -2,104 +2,46 @@ package Homework2;
 
 import java.util.*;
 
-//2. Создать класс “InitializationData” и метод внутри него, который будет предоставлять/наполнять данные для теста
-// функционала. Этот класс в итоге должен вернуть объект класса “Information”, с наполненными данными.
-// Условия наполнения:
-//2.1. Наполнить тридцатью уникальными значениями каждое из 3х полей класса “Information” (можно автоматически в цикле);
-//2.2. Добавить в каждое поле класса “Information” 10 одинаковых значений (Будьте внимательны при наполнении
-// Списка псевдонимов пользователя, так как эта коллекция не гарантирует уникальность значений);
-//2.3. При отдаче сущности “Information” созданного класса “InitializationData” необходимо чтобы размер 3х полей был
-// одинаковым(List.size() == Set.size() = Map.size()). Для данной проверки и корректировки, возможно,
-// Вам понадобиться дополнительный внутренний(private) метод.
-
-
 public class InitializationData {
+    //2. Создать класс “InitializationData” и метод внутри него, который будет предоставлять/наполнять данные для теста
+// функционала. Этот класс в итоге должен вернуть объект класса “Information”, с наполненными данными.
+
 
     //2.1. Наполнить тридцатью уникальными значениями каждое из 3х полей класса “Information” (можно автоматически в цикле);
-    public static ArrayList<String> userNameCreator(String name, int listLimit) {
-//        System.out.println("\n Печатаем nameList: \n");
-
-        ArrayList<String> nameList = new ArrayList<>();
+    public static Information informationInitData(Information information, String name, int listLimit) {
         for (int i = 1; i <= listLimit; i++) {
-            nameList.add(name + i);
+            information.getUserName().add(name + i);
+            information.getUserEmail().add(name + i + "gmail.com");
+            information.getUserData().put(String.valueOf(information.getUserName()), String.valueOf(information.getUserEmail()));
         }
-//        System.out.println(nameList);
-        return nameList;
+
+        return information;
 
     }
 
-    public static HashSet<String> userEmailCreator(String name, int listLimit) {
-//        System.out.println("\n Печатаем email Set: \n");
-        HashSet<String> emailList = new HashSet<>();
-        for (int i = 1; i <= listLimit; i++) {
-            emailList.add(name + i + "@gmail.com");
-        }
-//        System.out.println(emailList);
-        return emailList;
-    }
-
-    public static HashMap<String, String> userDataCreator(ArrayList arrayList, HashSet hashSet, int listLimit) {
-        Iterator iterator = hashSet.iterator();
-        HashMap<String, String> userData = new HashMap<>();
-        for (int i = 0; i < listLimit; i++) {
-            userData.put(String.valueOf(iterator.next()), (String) arrayList.get(i));
-
-        }
-//        System.out.println("\n Печатаем Hashmap: \n" + userData);
-        return userData;
-
-    }
 
     //2.2. Добавить в каждое поле класса “Information” 10 одинаковых значений (Будьте внимательны при наполнении
 // Списка псевдонимов пользователя, так как эта коллекция не гарантирует уникальность значений);
     //        Создаем еще два метода для добавления элементов
-    public static ArrayList<String> userNameCreator2(String name, int listLimit2) {
-        ArrayList<String> nameList = new ArrayList<>();
+
+    public static Information informationInitData2(Information information, String name, int listLimit2) {
+
         for (int i = 1; i <= listLimit2; i++) {
-            nameList.add(name);
+
+            if (!information.getUserName().contains(name)) {
+                information.getUserName().add(name);
+            }
+            information.getUserEmail().add(name + "mail.com");
+            information.getUserData().put(String.valueOf(information.getUserName()), String.valueOf(information.getUserEmail()));
+
+
         }
-//        System.out.println(nameList);
-        return nameList;
-
+        return information;
     }
+}
 
-    public static HashSet<String> userEmailCreator2(String name, int listLimit2) {
-
-        HashSet<String> emailList = new HashSet<>();
-        for (int i = 1; i <= listLimit2; i++) {
-            emailList.add(name + "@mail.com");
-        }
-//        System.out.println(emailList);
-        return emailList;
-    }
 //2.3. При отдаче сущности “Information” созданного класса “InitializationData” необходимо чтобы размер 3х полей был
 // одинаковым(List.size() == Set.size() = Map.size()). Для данной проверки и корректировки, возможно,
 // Вам понадобиться дополнительный внутренний(private) метод.
 
-    public static HashMap<String, String> checkLength(ArrayList arrayList, HashSet hashSet, int maxSize) {
-        for (int i = 0; i < arrayList.size(); i++) {
-            if (i >= maxSize) {
-                arrayList.remove(i);
-            }
 
-            for (int j = 0; j < hashSet.size(); j++) {
-                if (j >= maxSize) {
-                    arrayList.remove(j);
-                }
-
-            }
-
-        }
-        Iterator iterator = hashSet.iterator();
-        HashMap<String, String> userData = new HashMap<>();
-        for (int i = 0; i < maxSize; i++) {
-            userData.put(String.valueOf(iterator.next()), (String) arrayList.get(i));
-
-        }
-//        System.out.println("\n Печатаем Hashmap: \n" + userData);
-        return userData;
-
-    }
-
-
-}
--- src/main/java/Homework2/Main.java
+++ src/main/java/Homework2/Main.java
@@ -8,54 +8,31 @@ public class Main {
     public static void main(String[] args) {
         int listLimit = 30;
         int listLimit2 = 10;
-        int maxSize = 31;
         int randomSize = 10;
         String name1 = new String("John");
         String name2 = new String("Dan");
-
-//
-//        information.setUserName(userNameCreator(listLimit));
-//        information.setUserEmail(userEmailCreator(listLimit));
-
-        ArrayList<String> userNamelist = new ArrayList<>(userNameCreator(name1, listLimit));
-        System.out.println("\n userNamelist \n" + userNamelist);
-        HashSet<String> userEmailSet = new HashSet<>(userEmailCreator(name1, listLimit));
-        System.out.println("\n userEmailSet \n" + userEmailSet);
-
-        HashMap<String, String> usersHashMap = new HashMap<>(userDataCreator(userNamelist, userEmailSet, listLimit));
-        System.out.println("\n usersHashMap \n" + usersHashMap);
-
-        //2.2. Добавить в каждое поле класса “Information” 10 одинаковых значений (Будьте внимательны при наполнении
-// Списка псевдонимов пользователя, так как эта коллекция не гарантирует уникальность значений);
-//        Создаем еще два метода для добавления элементов
-
-        ArrayList<String> userNamelist2 = new ArrayList<>(userNameCreator2(name2, listLimit2));
-        System.out.println("\n userNamelist2 \n" + userNamelist2);
-        userNamelist.addAll(userNamelist2);
-        System.out.println("\n userNamelist Updated \n" + userNamelist);
-
-        HashSet<String> userEmailSet2 = new HashSet<>(userEmailCreator2(name2, listLimit2));
-        System.out.println("\n userEmailSet2 \n" + userEmailSet2);
-        userEmailSet.addAll(userEmailSet2);
-        System.out.println("\n userEmailSet Updated \n" + userEmailSet);
-
-        HashMap<String, String> usersHashMap2 = new HashMap<>(userDataCreator(userNamelist, userEmailSet, listLimit));
-        System.out.println("\n usersHashMap2 \n" + usersHashMap2);
 
 
 //2.3. При отдаче сущности “Information” созданного класса “InitializationData” необходимо чтобы размер 3х полей был
 // одинаковым(List.size() == Set.size() = Map.size()). Для данной проверки и корректировки, возможно,
 // Вам понадобиться дополнительный внутренний(private) метод.
-        HashMap<String, String> usersHashMap3 = new HashMap<>(checkLength(userNamelist, userEmailSet, maxSize));
-        System.out.println("\n usersHashMap3 \n" + usersHashMap3);
 
 
-        Information information = new Information(userNamelist, userEmailSet, usersHashMap3);
+        Information information = new Information();
+
+        informationInitData(information, name1, listLimit);
+        System.out.println("\n First method call \n" + informationInitData(information, name1, listLimit));
+
+        informationInitData2(information,name2,listLimit2);
+        System.out.println("\n Second method call \n" + informationInitData2(information,name2,listLimit2));
+
+
+//        создание списка "оберток"
         ArrayList<Integer> userToAdd = new ArrayList<Integer>(additionalUsersRandom(randomSize));
         System.out.println("\n userToAdd:\n" + userToAdd);
 
         System.out.println("\nInformation entity before adding: \n" + information);
-
+//      добавление новых юзеров к Классу Information:
         additionalUsers(userToAdd, information);
 
         System.out.println("\nInformation entity final: \n" + information);
@@ -69,36 +46,25 @@ public class Main {
 //        3.1. пройтись по всему списку переданному и преобразовав значение в строку записать в список псевдонимов
 //        пользователя;
 
-        ArrayList<String> nameList = new ArrayList<>();
-        for (int i = 0; i < userToAdd.size(); i++) {
-            userToAdd.get(i);
-            nameList.add(String.valueOf(i));
-        }
 
-//        3.2. Далее к данному значению добавить какое-нибудь заданное значение(например: @mail.ua) и записать в набор
+        for (int i = 0; i < userToAdd.size(); i++) {
+            information.getUserName().add(String.valueOf(userToAdd.get(i)));
+            //        3.2. Далее к данному значению добавить какое-нибудь заданное значение(например: @mail.ua) и записать в набор
 //        почт пользователя и Мапу их соотношения;
+            if (!information.getUserName().contains(String.valueOf(userToAdd.get(i)))) {//3.3. Так же необходимо
+                // реализовать проверку уже существующей почты в наборе и не записывать данное значение в случае совпадения
+                information.getUserEmail().add(String.valueOf(userToAdd.get(i)) + "@mail.com");
+            }
 
-        HashSet<String> emailSet = new HashSet<>();
-        for (int i = 0; i < userToAdd.size(); i++) {
-            emailSet.add(nameList.get(i) + "@mail.com");
-        }
-
-        Iterator iterator = emailSet.iterator();
-        HashMap<String, String> userData = new HashMap<>();
-        for (int i = 0; i < userToAdd.size(); i++) {
-            userData.put(String.valueOf(iterator.next()), nameList.get(i));
+            information.getUserData().put(String.valueOf(information.getUserEmail()), String.valueOf(userToAdd.get(i)));
 
         }
-        information.setUserName(nameList);
-        information.setUserEmail(emailSet);
-        information.setUserData(userData);
 
         return information;
     }
 
 
-//        3.3. Так же необходимо реализовать проверку уже существующей почты в наборе и не записывать данное значение в
-//        случае совпадения
+
 
 
     private static ArrayList<Integer> additionalUsersRandom(int n) { // метод для рандомного создания списка оберток
